b8e250027a4e8bd9c73813adc08a627076460078
PrabalKumar2311 <prabalhayden747@gmail.com>
2025-08-22 09:52:26 +0530

    fixed updaer fucntions for array and object

--- src/components/ArrayUpdate.jsx
+++ src/components/ArrayUpdate.jsx
@@ -1,41 +1,32 @@
-import {React, useState} from 'react'
+import React, { useState } from 'react'
 
 function ArrayUpdate() {
 
-  const [foods,setFoods] = useState(["Apple","Banana","Coconut"]);
-  const [newFood,setNewFoods] = useState("");
+  const [foods, setFoods] = useState(["Apple", "Banana", "Pineapple"]);
+  const [newFood, setNewFood] = useState("");
 
-
-  function handleInputChange(e){
-    setNewFoods(e.target.value);
+  function handleFoodChange(e){
+    setNewFood(e.target.value);
   }
 
-  function addFood(){
-    if(newFood.trim().length != 0){
-
-      console.log(foods);
-      //If i try to acces the updated state here it wont be shown because 
-      // the state is updated but the component has not rerendered yet 
-
-      //For printing the updated array then i have to first store it in a variable with the previous state and 
-      setFoods([...foods, newFood]);
-      const next = [...foods, newFood];
-      console.log('updated foods:', next);  
-
-    }
+  function handleAddFood(){
+    setFoods(foods => [...foods, newFood]);
   }
 
 
   return (
     <div>
-      {foods.map((food, index)=>{
+
+    <input type="text" value={newFood} onChange={handleFoodChange}/>
+    <button onClick={handleAddFood}>
+      Add
+    </button>
+
+      {foods.map((food,index) => {
         return(
           <li key={index}>{food}</li>
         )
       })}
-
-      <input type='text' value={newFood} onChange={handleInputChange}/>
-      <button onClick={addFood}>Add</button>
     </div>
   )
 }
